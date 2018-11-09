9043bb2f6e685d2e648d66e00ebee2f7ba2640d4
rashmiap <rashmiap.10@gmail.com>
2018-11-09 12:53:38 +0530

    request header api endpoint

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ var express = require('express');
 var app = express();
 
 // enable CORS (https://en.wikipedia.org/wiki/Cross-origin_resource_sharing)
-// so that your API is remotely testable by FCC 
+// so that your API is remotely testable by FCC
 var cors = require('cors');
 app.use(cors({optionSuccessStatus: 200}));  // some legacy browsers choke on 204
 
@@ -19,11 +19,19 @@ app.get("/", function (req, res) {
 });
 
 
-// your first API endpoint... 
+// your first API endpoint...
 app.get("/api/hello", function (req, res) {
   res.json({greeting: 'hello API'});
 });
 
+// request header API endpoint...
+app.get("/api/whoami", function (req, res) {
+  res.json({
+    ipaddress: req.headers["x-forwarded-for"].split(",")[0] || req.connection.remoteAddress,
+    language: req.headers["accept-language"],
+    software: req.headers["user-agent"]
+  });
+});
 
 
 // listen for requests :)
